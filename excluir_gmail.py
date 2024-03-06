11717779930c2d9c4a94adc4a0767edcd4dd1f96
Alisson027 <118004345+Alisson027@users.noreply.github.com>
2024-03-06 13:25:29 -0300

    Add files via upload

---
+++ excluir_gmail.py
@@ -0,0 +1,46 @@
+import imaplib
+import email
+from email.header import decode_header
+
+# Configurações do e-mail
+email_user = 'alissondsc17@gmail.com'
+email_pass = 'uectzubvmjgffhwo'
+
+# Conectar-se ao servidor IMAP usando imaplib
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+
+# Fazer login na conta
+mail.login(email_user, email_pass)
+
+# Selecionar a caixa de entrada (inbox)
+mail.select('inbox')
+
+# Buscar todos os e-mails que contenham o assunto" " ou corpo
+status, messages = mail.search(None, '(OR SUBJECT "PicPay" TEXT "picpay")')
+
+if status == 'OK':
+    email_ids = messages[0].split()
+    if not email_ids:
+        print("Nenhum e-mail encontrado com o termo 'Testes'.")
+    else:
+        # Excluir os e-mails
+        for email_id in email_ids:
+            mail.store(email_id, '+FLAGS', '(\Deleted)')
+
+        #excluir os e-mails marcados para exclusão
+        #O expunge exlcui definitivamente, nao manda para a lixeira
+        mail.expunge()
+        print("E-mails contendo 'teste' excluídos com sucesso.")
+
+# sair
+mail.logout()
+
+
+
+
+
+
+
+
+
+
